dfd51e643ce9469a8bbf6a2f210525444f139a7f
Ginepp <ginepp@gmail.com>
2022-05-23 12:13:54 -0500

    ok

--- src/lib/firebaseFunctions.js
+++ src/lib/firebaseFunctions.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// LINKS CONFIGURACIÓN
-export { app, db } from './config';
-
-// Link firebase
-export {
-  getAuth,
-  createUserWithEmailAndPassword,
-  sendEmailVerification,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
-}
-  // eslint-disable-next-line import/no-unresolved
-  from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
-
-// Link firestore
-export {
-  collection,
-  addDoc,
-  onSnapshot,
-  deleteDoc,
-  doc,
-  getDoc,
-// eslint-disable-next-line import/no-unresolved
-} from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
-=======
 /* eslint-disable import/no-unresolved */
 // Functions Firestore
 export {
@@ -39,4 +11,3 @@ export {
 // Functions Initialization
 export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js';
 export { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js';
->>>>>>> 4dc804f74e67a73616b1dd22ddac14736da911db
